521763e754b7b54ffd97616b2508d7c960a5c0cd
Alexey Voznyuk <me@swizard.info>
2020-06-25 00:05:06 +0300

    initial gen_server skeleton

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,51 @@
+use std::path::PathBuf;
 
+use futures::{
+    select,
+    stream,
+    channel::{
+        mpsc,
+        oneshot,
+    },
+    StreamExt,
+    SinkExt,
+};
 
+#[derive(Clone, Debug)]
+pub struct Params {
+    pub wheel_filename: PathBuf,
+    pub init_wheel_size_bytes: usize,
+}
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+pub struct GenServer {
+    request_tx: mpsc::Sender<Request>,
+    fused_request_rx: stream::Fuse<mpsc::Receiver<Request>>,
+}
+
+#[derive(Clone)]
+pub struct Pid {
+    request_tx: mpsc::Sender<Request>,
+}
+
+impl GenServer {
+    pub fn new() -> GenServer {
+        let (request_tx, request_rx) = mpsc::channel(0);
+        GenServer {
+            request_tx,
+            fused_request_rx: request_rx.fuse(),
+        }
+    }
+
+    pub fn pid(&self) -> Pid {
+        Pid {
+            request_tx: self.request_tx.clone(),
+        }
+    }
+
+    pub async fn run(self, params: Params) {
+
+        unimplemented!()
     }
 }
+
+struct Request;
