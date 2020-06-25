2117a3200bf910739ddbb31de65349452f1a5968
Alexey Voznyuk <me@swizard.info>
2020-06-26 02:13:27 +0300

    initial requests processing

--- src/wheel.rs
+++ src/wheel.rs
@@ -1,9 +1,11 @@
 use futures::{
+    select,
     stream,
     channel::{
         mpsc,
         oneshot,
     },
+    StreamExt,
 };
 
 use ero::{
@@ -43,7 +45,75 @@ pub struct State {
     pub params: Params,
 }
 
-pub async fn busyloop(state: State) -> Result<(), ErrorSeverity<State, ()>> {
+pub async fn busyloop(mut state: State) -> Result<(), ErrorSeverity<State, ()>> {
+    let mut blocks_pool = BlocksPool::new();
 
-    Ok(())
+    loop {
+        enum Source<A> {
+            Pid(A),
+        }
+        let req = select! {
+            result = state.fused_request_rx.next() =>
+                Source::Pid(result),
+        };
+        match req {
+            Source::Pid(None) => {
+                log::debug!("all Pid frontends have been terminated");
+                return Ok(());
+            },
+            Source::Pid(Some(Request::LendBlock { reply_tx, })) => {
+                let block = blocks_pool.lend();
+                if let Err(_send_error) = reply_tx.send(block) {
+                    log::warn!("Pid is gone during query result send");
+                }
+            },
+            Source::Pid(Some(Request::RepayBlock { block_bytes, })) => {
+                blocks_pool.repay(block_bytes);
+            },
+            Source::Pid(Some(Request::WriteBlock { block_bytes, reply_tx, })) => {
+
+                unimplemented!()
+            },
+            Source::Pid(Some(Request::ReadBlock { block_id, reply_tx, })) => {
+
+                unimplemented!()
+            },
+            Source::Pid(Some(Request::DeleteBlock { block_id, reply_tx, })) => {
+
+                unimplemented!()
+            },
+        }
+    }
+}
+
+struct BlocksPool {
+    pool: Vec<block::Bytes>,
+}
+
+impl BlocksPool {
+    fn new() -> BlocksPool {
+        BlocksPool {
+            pool: Vec::new(),
+        }
+    }
+
+    fn lend(&mut self) -> block::BytesMut {
+        let mut cursor = self.pool.len();
+        while cursor > 0 {
+            cursor -= 1;
+            let frozen_block = self.pool.swap_remove(cursor);
+            match frozen_block.into_mut() {
+                Ok(block) =>
+                    return block,
+                Err(frozen_block) =>
+                    self.pool.push(frozen_block),
+            }
+        }
+
+        block::BytesMut::new()
+    }
+
+    fn repay(&mut self, block_bytes: block::Bytes) {
+        self.pool.push(block_bytes)
+    }
 }
